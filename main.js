a6303a44d0e8d88fcbb6ff3ac064fe1e6049871d
Aaron Brelsford <aaronbrels@gmail.com>
2021-12-06 16:32:51 -0500

    Add the variable amountOfBombs and four new arrays as well as console logs to make sure they work.

--- main.js
+++ main.js
@@ -2,13 +2,26 @@ document.addEventListener("DOMContentLoaded", () =>{
 let grid = document.querySelector(".grid")
 let width = 10 // let javascript know that are grid is 10 squares width
 let squares = []
+let amountOfBombs = 20 
 
 
 // create the board
 function createBoard() {
-    for (let i = 0; i < width*width; i++) {
-       let square = document.createElement("div")
-       square.setAttribute("id", i)
+// get shuffled game array with random bombs
+let bombArray = Array(amountOfBombs).fill("bomb") // "Array" is an array constructor
+let emptyArray = Array(width*width - amountOfBombs).fill("valid")
+console.log(bombArray)
+console.log(emptyArray)
+let gamesArray = emptyArray.concat(bombArray)
+console.log(gamesArray)
+let shuffledArray = gamesArray.sort(function(){
+    return Math.random() -0.5
+})
+console.log(shuffledArray)
+
+for (let i = 0; i < width*width; i++) {  // create a 100 squares
+       let square = document.createElement("div") // create 100 elements for those 100 squares
+       square.setAttribute("id", i) // grab the square just created and give a unique id # when it loops through
        grid.appendChild(square)
        squares.push(square)
     }
