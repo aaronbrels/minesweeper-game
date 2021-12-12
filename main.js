1db364f413364c5c7a83993fa0b00fb3453aeb35
Aaron Brelsford <aaronbrels@gmail.com>
2021-12-12 16:30:38 -0500

    Added variable of total to for loop on line 29 and added several comments.

--- main.js
+++ main.js
@@ -26,12 +26,13 @@ for (let i = 0; i < width*width; i++) {  // create a 100 squares
 
     // add numbers
     for (let i = 0; i < squares.length; i++) {  // loop over the squares array
+        let total = 0
         let isLeftEdge = (i % width === 0)  // (if i or the number from 0 to 99), if for example 10 is divisible by the width which is 10 and leaves a remainder of zero this means that 10 is at the left edge
         let isRightEdge = (i === width -1) // 
 
 
         if (squares[i].classList.contains("valid")) { // If the square we are looping over contains the class of valid do the following
-            if (i > 0 && !isLeftEdge && squares[i -1].classList.contains("bomb")) total ++
+            if (i > 0 && !isLeftEdge && squares[i -1].classList.contains("bomb")) total ++  // if i is bigger than 0 and is not on the left edge and the squares to the left of it contain a bomb add 1 to the total | only works if all 3 parts are true
         }
     }
 
