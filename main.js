22ddac31a4b8fd5feb5403ee9bde10f7c19b53ac
Aaron Brelsford <aaronbrels@gmail.com>
2021-12-12 15:50:58 -0500

    Got rid of console.logs and added .classList.add on line 22.

--- main.js
+++ main.js
@@ -10,18 +10,16 @@ function createBoard() {
 // get shuffled game array with random bombs
 let bombArray = Array(amountOfBombs).fill("bomb") // "Array" is an array constructor
 let emptyArray = Array(width*width - amountOfBombs).fill("valid")
-console.log(bombArray)
-console.log(emptyArray)
 let gamesArray = emptyArray.concat(bombArray)
-console.log(gamesArray)
 let shuffledArray = gamesArray.sort(function(){
     return Math.random() -0.5
 })
-console.log(shuffledArray)
+
 
 for (let i = 0; i < width*width; i++) {  // create a 100 squares
        let square = document.createElement("div") // create 100 elements for those 100 squares
        square.setAttribute("id", i) // grab the square just created and give a unique id # when it loops through
+       square.classList.add(shuffledArray[i])
        grid.appendChild(square)
        squares.push(square)
     }
