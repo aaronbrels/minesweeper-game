f1b507a96ea4a3d204009332fc312ea85bce2df1
Aaron Brelsford <aaronbrels@gmail.com>
2021-12-12 16:21:44 -0500

    Added for loop and if statement and comments on lines 28 - 36.

--- main.js
+++ main.js
@@ -19,12 +19,30 @@ let shuffledArray = gamesArray.sort(function(){
 for (let i = 0; i < width*width; i++) {  // create a 100 squares
        let square = document.createElement("div") // create 100 elements for those 100 squares
        square.setAttribute("id", i) // grab the square just created and give a unique id # when it loops through
-       square.classList.add(shuffledArray[i])
+       square.classList.add(shuffledArray[i]) // Adding class names to the squares with bombs in them ?????
        grid.appendChild(square)
        squares.push(square)
     }
+
+    // add numbers
+    for (let i = 0; i < squares.length; i++) {  // loop over the squares array
+        let isLeftEdge = (i % width === 0)  // (if i or the number from 0 to 99), if for example 10 is divisible by the width which is 10 and leaves a remainder of zero this means that 10 is at the left edge
+        let isRightEdge = (i === width -1) // 
+
+
+        if (squares[i].classList.contains("valid")) { // If the square we are looping over contains the class of valid do the following
+            if (i > 0 && !isLeftEdge && squares[i -1].classList.contains("bomb")) total ++
+        }
+    }
+
+
+
+
+
+
 }
 createBoard()
+
 
 
 })
